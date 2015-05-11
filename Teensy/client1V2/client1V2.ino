4a68b889a2994947ef3a9a667f6f7df6150ea190
Jafo Teng <another0219@gmail.com>
2015-05-11 16:52:13 +1000

    Add in Teensy code

---
+++ Teensy/client1V2/client1V2.ino
@@ -0,0 +1,358 @@
+/**
+ * A Mirf example to test the latency between two Ardunio.
+ *
+ * Pins:
+ * Hardware SPI:
+ * MISO -> 12
+ * MOSI -> 11
+ * SCK -> 13
+ *
+ * Configurable:
+ * CE -> 8
+ * CSN -> 7
+ *
+ * Note: To see best case latency comment out all Serial.println
+ * statements not displaying the result and load 
+ * 'ping_server_interupt' on the server.
+ */
+#include <SPI.h>
+#include <Mirf.h>
+#include <nRF24L01.h>
+#include <MirfHardwareSpiDriver.h>
+
+const byte CLIENTNAME = (byte)0xC1;
+
+//Define command button pin
+const int buttonIF = 2;
+const int buttonTHEN = 3;
+const int buttonENTER = 4;
+const int buttonNOT = 5;
+const int buttonLOOP = 20;
+const int buttonOR = 22;
+
+// check the command is available to press
+boolean checkIF = true;
+boolean checkENTER = false;
+boolean checkTHEN = false;
+boolean checkNOT = false;
+boolean checkOR = false;
+boolean checkLOOP = true;
+
+// check the color is available to press
+boolean checkRED = false;
+boolean checkBLUE = false;
+boolean checkGREEN = false;
+boolean checkYELLOW = false;
+boolean checkPURPLE = false;
+
+boolean gameStart = false;
+boolean inputable = false;
+
+// check the command has been pressed
+boolean pressTHEN = false;
+boolean pressOR = false;
+boolean pressLOOP = false;
+
+//byte* test;
+//byte test[2];
+
+int currentState = 1;
+
+void setup(){
+  pinMode(buttonIF, INPUT);
+  pinMode(buttonTHEN, INPUT);
+  pinMode(buttonENTER, INPUT);
+  pinMode(buttonNOT, INPUT);
+  pinMode(buttonLOOP, INPUT);
+  pinMode(buttonOR, INPUT);
+
+  //test[0] = (byte)0xD5;
+  //test[1] = (byte)0xFF;
+
+  attachInterrupt(buttonIF, bState1, HIGH);
+  attachInterrupt(buttonTHEN, bState1, HIGH);
+  attachInterrupt(buttonENTER, bState1, HIGH);
+  attachInterrupt(buttonNOT, bState2, HIGH);
+  attachInterrupt(buttonLOOP, bState1, HIGH);
+  attachInterrupt(buttonOR, bState1, HIGH);
+
+  Serial.begin(9600);
+  
+  Mirf.spi = &MirfHardwareSpi;
+  Mirf.init();
+  Mirf.setRADDR((byte *)"clie1");
+  Mirf.payload = 32;
+  Mirf.config();
+
+  Serial.println("Beginning ... ");
+  
+}
+
+void loop(){
+  if(!Mirf.isSending() && Mirf.dataReady()){
+    byte data[Mirf.payload];
+    Mirf.getData(data);
+    Serial.print("Ping: ");
+    Serial.println(data[0],HEX);
+    dataGet(data[0]);
+  }
+  delay(100);
+} 
+//initial setting of the button
+void bState1(){
+  if(inputable){
+    if(digitalRead(buttonIF) == HIGH && checkIF){
+      IF();
+    }else if(digitalRead(buttonTHEN) == HIGH && checkTHEN){
+      THEN();
+    }else if(digitalRead(buttonENTER) == HIGH && checkENTER){
+      ENTER();
+    }else if(digitalRead(buttonLOOP) == HIGH && checkLOOP){
+      LOOP();
+    }else if(digitalRead(buttonOR) == HIGH && checkOR){
+      OR();
+    }
+    delay(500);
+  }
+}
+// color up, NOT up
+void bState2(){
+  if(inputable){
+    if(digitalRead(buttonIF) == HIGH && checkRED){
+       RED();
+    }else if(digitalRead(buttonTHEN) == HIGH && checkYELLOW){
+       YELLOW();
+    }else if(digitalRead(buttonENTER) == HIGH && checkGREEN){
+       GREEN();
+    }else if(digitalRead(buttonOR) == HIGH && checkPURPLE){
+       PURPLE();
+    }
+   
+    if(!pressLOOP){
+       if(digitalRead(buttonNOT) == HIGH && checkNOT){
+         NOT();
+       }else if(digitalRead(buttonLOOP) == HIGH && checkBLUE){
+         BLUE();
+       }
+     }else{
+       if(digitalRead(buttonNOT) == HIGH && checkBLUE){
+         BLUE();
+       }
+     }
+  }
+}
+
+void stateChange(int toState){
+  checkReset();
+  
+  if (toState == 1){
+    // IF / LOOP
+    buttonChange(1);
+    checkIF = true;
+    checkLOOP = true;
+  }else if(toState == 2){
+    // COLOR / NOT
+    buttonChange(2);
+    if(!gameStart){
+      checkNOT = true;
+      checkRED = true;
+      checkBLUE = true;
+      checkGREEN = true;
+      checkYELLOW = true;
+      checkPURPLE = true;
+    }
+  }else if(toState == 3){
+    // THEN / OR / ENTER
+    buttonChange(1);
+    if(!pressLOOP){
+      if(!pressTHEN){
+        checkTHEN = true;
+      }else{
+        checkENTER = true;
+      }
+      if(!pressOR){
+        checkOR = true;
+      }
+    }else{
+      checkTHEN = true;
+      if(pressTHEN){
+        checkENTER = true;
+      }
+    }
+  }
+}
+
+void buttonChange(int temp){
+  delay(500);
+  if(temp == 1){
+    attachInterrupt(buttonIF, bState1, HIGH);
+    attachInterrupt(buttonTHEN, bState1, HIGH);
+    attachInterrupt(buttonENTER, bState1, HIGH);
+    attachInterrupt(buttonLOOP, bState1, HIGH);
+    attachInterrupt(buttonOR, bState1, HIGH);
+  }else if(temp == 2){
+    attachInterrupt(buttonIF, bState2, HIGH);
+    attachInterrupt(buttonTHEN, bState2, HIGH);
+    attachInterrupt(buttonENTER, bState2, HIGH);
+    attachInterrupt(buttonLOOP, bState2, HIGH);
+    attachInterrupt(buttonOR, bState2, HIGH);
+  }
+}
+
+void IF(){
+  Serial.println("IF");
+  byte str = (byte)0x01; 
+  dataSending(str);
+  stateChange(2);
+}
+
+void THEN(){
+  Serial.println("THEN");
+  byte str = (byte)0x02;
+  dataSending(str);
+  
+  pressTHEN = true;
+  pressOR = false;
+  stateChange(2);
+}
+
+void NOT(){
+  Serial.println("NOT");
+  byte str = (byte)0x03;
+  dataSending(str);
+  checkNOT = false;
+}
+
+void LOOP(){
+  Serial.println("LOOP");
+  byte str = (byte)0x04;
+  dataSending(str);
+  
+  pressLOOP = true;
+  checkLOOP = false;
+  
+  // LED twinkle 
+}
+void OR(){
+  Serial.println("OR");
+  byte str = (byte)0x05;  
+  dataSending(str);
+  pressOR = true;
+  stateChange(2);
+}
+void RED(){
+  Serial.println("RED");
+  byte str = (byte)0x06;  
+  dataSending(str);
+  if(!gameStart){
+    stateChange(3);
+  }else{
+    checkReset();
+  }
+}
+void BLUE(){
+  Serial.println("BLUE");
+  byte str = (byte)0x07;  
+  dataSending(str);
+  if(!gameStart){
+    stateChange(3);
+  }else{
+    checkReset();
+  }
+}
+void GREEN(){
+  Serial.println("GREEN");
+  byte str = (byte)0x08;  
+  dataSending(str);
+  if(!gameStart){
+    stateChange(3);
+  }else{
+    checkReset();
+  }
+}
+void YELLOW(){
+  Serial.println("YELLOW");
+  byte str = (byte)0x09;  
+  dataSending(str);
+  if(!gameStart){
+    stateChange(3);
+  }else{
+    checkReset();
+  }
+}
+void PURPLE(){
+  Serial.println("PURPLE");
+  byte str = (byte)0x0A;  
+  dataSending(str);
+  if(!gameStart){
+    stateChange(3);
+  }else{
+    checkReset();
+  }
+}
+void ENTER(){
+  Serial.println("ENTER");
+  byte str = (byte)0x0B;  
+  dataSending(str);
+  stateChange(1);
+  checkENTER = false;
+  pressTHEN = false;
+  pressOR = false;
+  if(pressLOOP){
+    //stop twinkle
+    pressLOOP = false;
+  }
+}
+// data sending through radio
+void dataSending(byte str){
+  byte data[2] = {CLIENTNAME, str};
+  Mirf.setTADDR((byte *)"serv1");
+  Mirf.send(data);
+  while(Mirf.isSending()){
+  }
+  Serial.println("Data Sent");
+}
+void dataGet(byte str){
+  if(str == (byte)0xFF){
+    gameStart = true;
+    stateChange(2);
+  }else if(str == (byte)0xEE){
+    inputable = true;
+  }else if(str == (byte)0xDD){
+    inputable = false;
+    gameStart = false;
+    stateChange(1);
+  }
+  
+  if(str == (byte)0x06){
+    checkRED = true;
+    Serial.println("LED:RED");
+  }else if(str == (byte)0x07){
+    checkBLUE = true;
+    Serial.println("LED:BLUE");
+  }else if(str == (byte)0x08){
+    checkGREEN = true;
+    Serial.println("LED:GREEN");
+  }else if(str == (byte)0x09){
+    checkYELLOW = true;
+    Serial.println("LED:YELLOW");
+  }else if(str == (byte)0x0A){
+    checkPURPLE = true;
+    Serial.println("LED:PURPLE");
+  }
+}
+
+void checkReset(){
+  checkIF = false;
+  checkENTER = false;
+  checkTHEN = false;
+  checkNOT = false;
+  checkOR = false;
+  checkLOOP = false;
+  
+  checkRED = false;
+  checkBLUE = false;
+  checkGREEN = false;
+  checkYELLOW = false;
+  checkPURPLE = false;
+}
---
+++ Teensy/serverV2/serverV2.ino
@@ -0,0 +1,143 @@
+/**
+ * An Mirf example which copies back the data it recives.
+ *
+ * Pins:
+ * Hardware SPI:
+ * MISO -> 12
+ * MOSI -> 11
+ * SCK -> 13
+ *
+ * Configurable:
+ * CE -> 8
+ * CSN -> 7
+ *
+ */
+
+#include <SPI.h>
+#include <Mirf.h>
+#include <nRF24L01.h>
+#include <MirfHardwareSpiDriver.h>
+
+const int numOfClient = 3;
+
+char serialData[20];
+char* clients[numOfClient] = {"clie1", "clie2", "clie3"};
+
+
+void setup(){
+  Serial.begin(9600);
+  Mirf.spi = &MirfHardwareSpi;
+  Mirf.init();
+  Mirf.setRADDR((byte *)"serv1");
+  Mirf.payload = 32;
+  Mirf.config();
+  //Serial.println("Listening..."); 
+
+}
+
+void loop(){
+  
+
+  if(!Mirf.isSending() && Mirf.dataReady()){
+    //Serial.println("Get data");
+    byte data[Mirf.payload];
+    Mirf.getData(data);
+    String from = "";
+    String str = "";
+    if(data[0] == (byte)0xC1){
+      from = "C1";
+    }else if(data[0] == (byte)0xC2){
+      from = "C2";
+    }else if(data[0] == (byte)0xC3){
+      from = "C3";
+    }
+    
+    if(data[1] == (byte)0x01){
+      str = "I"; //IF
+    }else if(data[1] == (byte)0x02){
+      str = "T"; //THEN
+    }else if(data[1] == (byte)0x03){
+      str = "N"; //NOT
+    }else if(data[1] == (byte)0x04){
+      str = "L"; //LOOP
+    }else if(data[1] == (byte)0x05){
+      str = "O"; //OR
+    }else if(data[1] == (byte)0x06){
+      str = "R"; //RED
+    }else if(data[1] == (byte)0x07){
+      str = "B"; //BLUE
+    }else if(data[1] == (byte)0x08){
+      str = "G"; //GREEN
+    }else if(data[1] == (byte)0x09){
+      str = "Y"; //YELLOW
+    }else if(data[1] == (byte)0x0A){
+      str = "P"; //PURPLE
+    }else if(data[1] == (byte)0x0B){
+      str = "E"; //ENTER
+    }
+    Serial.println(from + ":" + str);
+    //Serial.println(data[1], HEX);
+    Serial.flush();
+  }
+  
+  //read from Serial
+  if(Serial.available() > 0){
+    int lf = 10;
+    Serial.readBytesUntil(lf,serialData,11);
+    char* client;
+    if(strstr(serialData, "C1") != NULL){
+      client = "clie1";
+    }else if(strstr(serialData, "C2") != NULL){
+      client = "clie2";
+    }else if(strstr(serialData, "C3") != NULL){
+      client = "clie3";
+    }
+    
+    if(strstr(serialData,"GameStart") != NULL){
+      int x;
+      for(x = 0; x < numOfClient; x++){
+        sendData(clients[x], "GameStart");
+      }
+    }else if(strstr(serialData,"StartInput") != NULL){
+      int x;
+      for(x = 0; x < numOfClient; x++){
+        sendData(clients[x], "StartInput");
+      }
+    }else if(strstr(serialData,"GameFin") != NULL){
+      int x;
+      for(x = 0; x < numOfClient; x++){
+        sendData(clients[x], "GameFin");
+      }
+    }else{
+      sendData(client, serialData);
+    }
+    Serial.flush();
+  }
+}
+
+
+void sendData(char* client, char* sendingData){
+  byte sendData[1];
+
+  if(strstr(sendingData, "GameStart") != NULL){
+    sendData[0] = (byte)0xFF;
+  }else if(strstr(sendingData, "StartInput") != NULL){
+    sendData[0] = (byte)0xEE;
+  }else if(strstr(sendingData, "GameFin") != NULL){
+    sendData[0] = (byte)0xDD;
+  }else if(strstr(sendingData, "R") != NULL){
+    sendData[0] = (byte)0x06;
+  }else if(strstr(sendingData, "B") != NULL){
+    sendData[0] = (byte)0x07;
+  }else if(strstr(sendingData, "G") != NULL){
+    sendData[0] = (byte)0x08;
+  }else if(strstr(sendingData, "Y") != NULL){
+    sendData[0] = (byte)0x09;
+  }else if(strstr(sendingData, "P") != NULL){
+    sendData[0] = (byte)0x0A;
+  }
+  Mirf.setTADDR((byte *)client);
+  Mirf.send(sendData);
+  while(Mirf.isSending()){
+  }
+}
