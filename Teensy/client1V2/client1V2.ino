0958da09e76f291e9d44d80d93ee90a79df918aa
Jafo Teng <another0219@gmail.com>
2015-09-05 04:27:07 +1000

    test3

--- Teensy/client1V2/client1V2.ino
+++ Teensy/client1V2/client1V2.ino
@@ -1,6 +1,6 @@
 /**
- * A Mirf example to test the latency between two Ardunio.2345678vghjkljhgfds
- *asdfghjkjhgfdsdfghjk
+ * A Mirf example to test the latency between two Ardunio.23456787654345678
+ *`1234567898765432456789ertyuioihgfd
  * Pins:
  * Hardware SPI:
  * MISO -> 12
